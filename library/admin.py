f5d968f8038b6d5b45880bb7b002566048915f4a
Doczadrot <doczadrot@example.com>
2025-06-05 14:59:16 +0300

    Добавляем фикстуры

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Autor, Book # Убедитесь, что Autor и Book импортированы правильно
 
-# Register your models here.
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'birth_date')
+    search_fields = ('first_name', 'last_name')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publication_date', 'autor') # <-- ИЗМЕНЕНО!
+    list_filter = ('publication_date', 'autor')          # <-- ИЗМЕНЕНО!
+    search_fields = ('title', 'autor__first_name', 'autor__last_name')
