18a1f739a37e097ecedc71784b7f89f79e6c1509
Dockzadrot <126706920+Dockzadrot@users.noreply.github.com>
2025-06-05 21:51:03 +0300

    иксация изменений перед обновлением

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,13 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Autor, Book  # Импортируем наши модели
+
+@admin.register(Book) # Декоратор для регистрации модели Автор в административной панели
+class BookAdmin(admin.ModelAdmin):
+    """
+    Настройки отображения модели Книга в административной панели
+    """
+    list_display = ('title', 'autor', 'publication_date') # Поля для отображения в списке записей
+# admin.site.register(Autor) # Регистрируем модель Автор в админке для управления данными
+# admin.site.register(Book) # Регистрируем модель Книга в админке для управления данными
+# # Register your models here.
