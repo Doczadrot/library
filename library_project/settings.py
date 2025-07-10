29b2ded1e160813482b1f7d9b005aed4072197a0
Dockzadrot <155732866+Doczadrot@users.noreply.github.com>
2025-07-10 20:07:59 +0300

    ВЫполнили  задачи:Создайте приложение.Откатите миграции приложения auth, Создайте кастомную модель пользователя.Настройте проект для использования кастомной модели пользователя.

--- library_project/settings.py
+++ library_project/settings.py
@@ -1,5 +1,7 @@
 
 from pathlib import Path
+
+from django.conf.global_settings import AUTH_USER_MODEL
 from dotenv import load_dotenv
 import os
 
@@ -32,7 +34,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'library'
+    'library',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +129,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
