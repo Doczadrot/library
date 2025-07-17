25dc40f523660e72d8a8489f7f3eb5272e34d0b3
Dockzadrot <155732866+Doczadrot@users.noreply.github.com>
2025-07-17 21:38:50 +0300

    Добавили разные права пользователям через  Добавили груповые права

--- library/admin.py
+++ library/admin.py
@@ -1,21 +1,17 @@
-# C:\Users\ivan\Desktop\LybruryDjango\library\admin.py
 
 from django.contrib import admin
-from .models import Autor, Book # Импортируем обе модели ОДИН РАЗ, в начале файла
+from .models import Autor, Book
 
-# --- Регистрация модели Autor ---
+
 @admin.register(Autor) # Регистрируем Autor
 class AutorAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'birth_date')
     search_fields = ('first_name', 'last_name')
 
-# --- Регистрация модели Book ---
-# Должен быть ТОЛЬКО ОДИН класс BookAdmin с @admin.register(Book)
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    """
-    Административная панель для управления книгами
-    """
     # Используйте правильные имена полей, как определено в models.py
     # publication_date - это ваше поле с датой публикации в models.py
     list_display = ('title', 'autor', 'publication_date')
